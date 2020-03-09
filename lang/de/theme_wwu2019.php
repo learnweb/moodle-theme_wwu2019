53e7bb813f833ba0ce66639e0d16b4cf90e28de9
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2020-03-09 16:19:55 +0100

    Übersetzungen

---
+++ lang/de/theme_wwu2019.php
@@ -0,0 +1,83 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * WWU 2019
+ *
+ * @package    theme_wwu2019
+ * @copyright  2019 Justus Dieckmann WWU
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'WWU 2019';
+$string['choosereadme'] = 'Dieses Theme basiert auf dem Classic-Theme von Moodle und ist hinsichtlich des Corporate Design der WWU Münster angepasst.';
+
+/* Main menu */
+$string['mycourses'] = 'Meine Kurse';
+$string['termindependent'] = 'Semesterunabhängig';
+$string['dashboard'] = 'Dashboard';
+
+$string['thiscourse'] = 'Dieser Kurs';
+$string['searchadmin'] = 'Administrationssuche';
+
+/* User menu */
+$string['loggedinas'] = '{$a->real}, als {$a->fake}';
+$string['badgepreferences'] = 'Badge';
+$string['mygrades'] = 'Meine Noten';
+
+/* Settings */
+$string['helpurl'] = 'Hilfe-URL';
+$string['helpurl_desc'] = 'URL zu einer Hilfeseite, die im Usermenü verlinkt ist.';
+$string['matomo_siteurl'] = 'Analytics-URL';
+$string['matomo_siteurl_desc'] = 'Ihre "Matomo Analytics"-URL, aber ohne http(s) oder Slash am Ende. Beispiel: "mysite.com/analytics".';
+$string['matomo_siteid'] = 'Analytics-Site-ID';
+$string['matomo_siteid_desc'] = 'Ihre "Matomo Analytics" Site-ID.';
+$string['enable1alert'] = 'Alert 1 zeigen';
+$string['enable2alert'] = 'Alert 2 zeigen';
+$string['enable3alert'] = 'Alert 3 zeigen';
+$string['enablealert_desc'] = 'Ob der jeweilige Alert angezeigt wird.';
+$string['alert1type'] = 'Alert-Typ (Alert 1)';
+$string['alert1title'] = 'Alert-Titel (Alert 1)';
+$string['alert1text'] = 'Alert-Text (Alert 1)';
+$string['alert2type'] = 'Alert-Typ (Alert 2)';
+$string['alert2title'] = 'Alert-Titel (Alert 2)';
+$string['alert2text'] = 'Alert-Text (Alert 2)';
+$string['alert3type'] = 'Alert-Typ (Alert 3)';
+$string['alert3title'] = 'Alert-Titel (Alert 3)';
+$string['alert3text'] = 'Alert-Text (Alert 3)';
+$string['alerttype_desc'] = 'Typ des Alerts (vgl. debug levels).';
+$string['alerttitle_desc'] = 'Erster Teil der Meldung (wird fettgedruckt).';
+$string['alerttext_desc'] = 'Zweiter Teil der Meldung.';
+
+
+$string['alert_info'] = 'Info';
+$string['alert_warning'] = 'Warnung';
+$string['alert_general'] = 'Ankündigung';
+
+// Marketing Spots.
+$string['marketingheading'] = 'Marketing spots';
+$string['marketinginfodesc'] = 'Enter the settings for your marketing spot.';
+$string['marketingdesc'] = 'This theme provides the option of enabling three "marketing" or "ad" spots just under the slide show.  These allow you to easily identify core information to your users and provide direct links.';
+$string['marketing1'] = 'Marketing spot one';
+$string['marketing2'] = 'Marketing spot two';
+$string['marketing3'] = 'Marketing spot three';
+
+$string['marketingtitle'] = 'Titel';
+$string['marketingtitledesc'] = 'Title to show in this marketing spot';
+$string['marketingcontent'] = 'Beschreibung';
+$string['marketingcontentdesc'] = 'Content to display in the marketing box.  Keep it short and sweet.';
