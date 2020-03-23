c29dd430e53f535dfe359f0dd84dbbda71ea5a41
Justus Dieckmann <justusdieckmann@wwu.de>
2020-03-23 21:10:52 +0100

    Sort courses for each semester alphabetically

--- classes/output/core_renderer.php
+++ classes/output/core_renderer.php
@@ -486,7 +486,7 @@ class core_renderer extends \core_renderer {
                                 INNER JOIN mdl_customfield_data as cd ON cs.id=cd.instanceid
                                 WHERE cs.id ' . $instring . '
                                 AND cd.fieldid = :fieldid
-                                ORDER BY cs.startdate DESC';
+                                ORDER BY cd.value DESC, cs.shortname ASC';
         $params['fieldid'] = $fieldid;
         return $DB->get_records_sql($fromtable, $params);
     }
